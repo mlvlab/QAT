e4268b85a11fa403605d1bb0c614271eb51da96d
PJin0 <lpmn678@korea.ac.kr>
2023-03-10 14:02:47 +0900

    setup file

---
+++ setup.sh
@@ -0,0 +1,9 @@
+pip install torch==1.8.1+cu102 -f https://download.pytorch.org/whl/cu102/torch_stable.html
+pip install torch-scatter==2.0.6 -f https://data.pyg.org/whl/torch-1.8.1+cu102.html
+pip install torch-sparse==0.6.9 -f https://data.pyg.org/whl/torch-1.8.1+cu102.html
+pip install torch-cluster==1.5.9 -f https://data.pyg.org/whl/torch-1.8.1+cu102.html
+pip install torch-spline-conv==1.2.1 -f https://data.pyg.org/whl/torch-1.8.1+cu102.html
+pip install torch-geometric==1.7.0 -f https://data.pyg.org/whl/torch-1.8.1+cu102.html
+pip install transformers==2.2.0
+pip install nltk spacy sentencepiece numpy tqdm tensorboard
+python -m spacy download en
